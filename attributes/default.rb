226093dc3476b3886f6c1806c3a60ddd7fbc261f
syucream <syucream1031@gmail.com>
2014-01-22 00:52:23 +0900

    Fix attributes of record.config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,8 @@
 default['trafficserver']['install_method'] = 'git-repo'
 
 # attributes for config file
+default['trafficserver']['record']['proxy_name'] = "trafficserver"
 default['trafficserver']['records']['port'] = "8080"
+default['trafficserver']['record']['logdir'] = "var/log/trafficserver"
+default['trafficserver']['record']['sysconfdir'] = "etc/trafficserver"
 default['trafficserver']['remap']['map'] = [{ 'cache' => 'http://cache.example.com/', 'origin' => 'http://origin.example.com/' }]
