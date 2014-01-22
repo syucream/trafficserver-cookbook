c45a754df699f3ce56ef037febff498da440dcb2
syucream <syucream1031@gmail.com>
2014-01-22 23:07:56 +0900

    Fix attribute name of remap.config

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,6 @@ default['trafficserver']['records']['logdir'] = "var/log/trafficserver"
 default['trafficserver']['records']['certification_level'] = "0"
 default['trafficserver']['records']['sysconfdir'] = "etc/trafficserver"
 ## remap.config
-default['trafficserver']['remap']['map'] = [{ 'cache' => 'http://cache.example.com/', 'origin' => 'http://origin.example.com/' }]
+default['trafficserver']['remap']['map'] = [{ 'ts' => 'http://cache.example.com/', 'origin' => 'http://origin.example.com/' }]
 ## ssl_multicert.config
 default['trafficserver']['ssl_multicert'] = [{ 'dest_ip' => '*', 'ssl_cert_name' => 'server.crt', 'ssl_key_name' => 'server.key' }]
