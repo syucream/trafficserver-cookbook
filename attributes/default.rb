eb3ee164fa5e2d0335ac85af5188be7959fb8a7f
syucream <syucream1031@gmail.com>
2014-01-22 22:14:19 +0900

    Add "certification_level" attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,6 @@ default['trafficserver']['install_method'] = 'git-repo'
 default['trafficserver']['record']['proxy_name'] = "trafficserver"
 default['trafficserver']['records']['port'] = "8080"
 default['trafficserver']['record']['logdir'] = "var/log/trafficserver"
+default['trafficserver']['record']['certification_level'] = "0"
 default['trafficserver']['record']['sysconfdir'] = "etc/trafficserver"
 default['trafficserver']['remap']['map'] = [{ 'cache' => 'http://cache.example.com/', 'origin' => 'http://origin.example.com/' }]
