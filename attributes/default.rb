c7a02c1ce6359cbb91dbaea44ff3690f7b21a709
syucream <syucream1031@gmail.com>
2014-01-22 23:03:26 +0900

    Fix name of attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,11 +2,11 @@ default['trafficserver']['install_method'] = 'git-repo'
 
 # attributes for config file
 ## records.config
-default['trafficserver']['record']['proxy_name'] = "trafficserver"
+default['trafficserver']['records']['proxy_name'] = "trafficserver"
 default['trafficserver']['records']['server_ports'] = "8080"
-default['trafficserver']['record']['logdir'] = "var/log/trafficserver"
-default['trafficserver']['record']['certification_level'] = "0"
-default['trafficserver']['record']['sysconfdir'] = "etc/trafficserver"
+default['trafficserver']['records']['logdir'] = "var/log/trafficserver"
+default['trafficserver']['records']['certification_level'] = "0"
+default['trafficserver']['records']['sysconfdir'] = "etc/trafficserver"
 ## remap.config
 default['trafficserver']['remap']['map'] = [{ 'cache' => 'http://cache.example.com/', 'origin' => 'http://origin.example.com/' }]
 ## ssl_multicert.config
