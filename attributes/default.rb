a9392b5dee279b63ffa1abcf394e5a2af508d075
syucream <syucream1031@gmail.com>
2014-01-22 22:45:57 +0900

    Add template of ssl_multicert.config

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,13 @@
 default['trafficserver']['install_method'] = 'git-repo'
 
 # attributes for config file
+## records.config
 default['trafficserver']['record']['proxy_name'] = "trafficserver"
 default['trafficserver']['records']['server_ports'] = "8080"
 default['trafficserver']['record']['logdir'] = "var/log/trafficserver"
 default['trafficserver']['record']['certification_level'] = "0"
 default['trafficserver']['record']['sysconfdir'] = "etc/trafficserver"
+## remap.config
 default['trafficserver']['remap']['map'] = [{ 'cache' => 'http://cache.example.com/', 'origin' => 'http://origin.example.com/' }]
+## ssl_multicert.config
+default['trafficserver']['ssl_multicert'] = [{ 'dest_ip' => '*', 'ssl_cert_name' => 'server.crt', 'ssl_key_name' => 'server.key' }]
